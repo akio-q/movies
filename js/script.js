512c023f02be4b404ce50c720f5261200462ac4a
Valentyn <wolf.valentyn@gmail.com>
2022-07-28 16:37:42 +0300

    add functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 "use strict";
 
-const numberOfFilms = +prompt('How many movies have you watched already?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many movies have you watched already?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many movies have you watched already?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,23 +20,33 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('One of the last movies you watched?', ''),
-          b = prompt('How much would you rate it?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-    } else {
-        i--;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('One of the last movies you watched?', ''),
+              b = prompt('How much would you rate it?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
     }
 }
 
-if (personalMovieDB.count <= 10) {
-    alert('You watched quite a bit films');
-} else if (personalMovieDB.count < 30) {
-    alert('You are typical viewer');
-} else if (personalMovieDB.count >= 30) {
-    alert('You are a true cinephile!');
-} else {
-    alert('Error');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count <= 10) {
+        console.log('You watched quite a bit films');
+    } else if (personalMovieDB.count < 30) {
+        console.log('You are typical viewer');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('You are a true cinephile!');
+    } else {
+        console.log('Error');
+    }
 }
+
+detectPersonalLevel();
+
+console.log(personalMovieDB);
